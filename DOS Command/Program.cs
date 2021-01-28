76685c7af64fd27bd3f6e609ad093b4e670aa86b
Banee Ishaque K <baneeishaque@gmail.com>
2021-01-29 02:28:36 +0530

    In

---
+++ DOS Command/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace DOS_Command
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                ProcessStartInfo a = new ProcessStartInfo("cmd", "/c echo hai");
+                a.RedirectStandardOutput = true;
+                a.UseShellExecute = false;
+                a.CreateNoWindow = true;
+                Process b = new Process();
+                b.StartInfo = a;
+                b.Start();
+                Console.WriteLine(b.StandardOutput.ReadToEnd());
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+
+            }
+
+        }
+    }
+}
